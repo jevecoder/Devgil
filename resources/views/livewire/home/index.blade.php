8de3693af48d384e6bcbae058d3c94dcaff82a5f
BegDev <83625997+jevecoder@users.noreply.github.com>
2023-07-09 18:57:42 +0800

    progress bar update

---
+++ resources/views/livewire/home/index.blade.php
@@ -0,0 +1,213 @@
+
+    <div class="flex flex-col h-screen w-full bg-[#0A0909]">
+
+        <div class="flex flex-row h-6 p-1 font-mono text-xs font-bold bg-slate-300">
+            <div class="w-24 h-24 pl-20"><img src="{{ asset('storage/images/icon/phoneicon.svg')}}" alt=""></div>
+            <div class="pl-2">0916-5345-324</div>
+            <div class="pl-3">0916|5345|324</div>
+          </div>
+
+
+
+
+
+          <div class="flex h-20 text-white bg-black justify-evenly place-items-center">
+            <a href="#" class="text-white transition duration-300 group">
+                HOME
+                <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+                </a>
+
+                <a href="#" class="text-white transition duration-300 group">
+                    SERVICES
+                    <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+                    </a>
+              <div class="text-3xl font-bold">BegDev</div>
+              <a href="#" class="text-white transition duration-300 group">
+                SKILLS
+                <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+                </a>
+                <a href="#" class="text-white transition duration-300 group">
+                    HOME
+                    <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-sky-600"></span>
+                    </a>
+
+            </div>
+
+
+
+
+
+
+
+
+            <div class="flex flex-row justify-center place-items-center">
+                <P class="absolute font-mono text-4xl font-bold text-white left-52 top-64">Whoever you are, find <br>whatever you”re into</P>
+
+    <div class="absolute left-52 top-96">
+
+<form>
+    <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+    <div class="relative">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+            <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+            </svg>
+        </div>
+        <input type="search" id="default-search" class="block p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-96 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="What are you looking for?" required>
+        <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-[#FF9900] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+    </div>
+</form>
+
+                    </div>
+                <div class="mt-20 bg-center bg-no-repeat "><img src="{{ asset('storage/images/bg.svg')}}" alt=""></div>
+        </div>
+
+
+
+
+
+
+
+</div>
+
+
+
+<div class="flex flex-col justify-center h-screen w-full bg-[#0A0909]">
+  
+<div class="flex flex-row w-full">
+
+<div class="flex flex-col justify-center gap-5 items-center w-screen h-80 pb-56" style="padding-left:10%">
+<div class="absolute text-b text-white" style="margin-bottom:4.5%;margin-right:34.3%">UX DESIGN</div><div class="bg-gray-200 rounded-full h-2.5 dark:bg-[#D9D9D9] w-4/6">
+  <div class="bg-[#0D768D] h-2.5 rounded-full" style="width: 45%"></div>
+</div>
+<div class="absolute text-b text-white" style="margin-bottom:0.2%;margin-right:36.6%">VISUAL DESIGN</div><div class="w-4/6 bg-gray-200 rounded-full h-2.5 dark:bg-[#D9D9D9]">
+  <div class="bg-[#0D768D] h-2.5 rounded-full" style="width: 45%"></div>
+</div>
+<div class="absolute text-b text-white" style="margin-bottom:-4.4%;margin-right:38%">BUSINESS DESIGN</div><div class="w-4/6 bg-gray-200 rounded-full h-2.5 dark:bg-[#D9D9D9]">
+  <div class="bg-[#0D768D] h-2.5 rounded-full" style="width: 45%"></div>
+</div>
+</div>
+
+
+
+<div class="flex flex-col justify-center gap-5 items-center w-full h-80 pb-56" style="padding-right:7%">
+<div class="absolute text-b text-white" style="margin-bottom:-0.1%;margin-right:40%">BRANDING DESIGN</div><div class="bg-gray-200 rounded-full h-2.5 dark:bg-[#D9D9D9] w-4/6">
+  <div class="bg-[#0D768D] h-2.5 rounded-full" style="width: 45%"></div>
+</div>
+<div class="absolute text-b text-white" style="margin-bottom:4.5%;margin-right:39.8%">MOTION GRAPHIC</div><div class="w-4/6 bg-gray-200 rounded-full h-2.5 dark:bg-[#D9D9D9]">
+  <div class="bg-[#0D768D] h-2.5 rounded-full" style="width: 45%"></div>
+</div>
+<div class="absolute text-b text-white" style="margin-bottom:-4.4%;margin-right:38.1%">FLYERS DESIGN</div><div class="w-4/6 bg-gray-200 rounded-full h-2.5 dark:bg-[#D9D9D9]">
+  <div class="bg-[#0D768D] h-2.5 rounded-full" style="width: 45%"></div>
+</div>
+</div>
+
+</div>
+
+<div class="text-white font-bold text-base self-center absolute">MORE SKILLS</div>
+
+<div class=" flex flex-row justify-center">
+<div class="grid grid-cols-1 gap-20 lg:grid-cols-2 lg:gap-10">
+    <div class="flex items-center flex-wrap max-w-md px-10  rounded-2xl h-20"
+       x-data="{ circumference: 50 * 2 * Math.PI, percent: 80 }"
+       >
+          
+            <svg class="w-32 h-32 transform translate-x-1 translate-y-1" x-cloak aria-hidden="true">
+              <circle
+                class="text-gray-300"
+                stroke-width="10"
+                stroke="currentColor"
+                fill="transparent"
+                r="50"
+                cx="60"
+                cy="60"
+                />
+              <circle
+                class="text-blue-600"
+                stroke-width="10"
+                :stroke-dasharray="circumference"
+                :stroke-dashoffset="circumference - percent / 100 * circumference"
+                stroke-linecap="round"
+                stroke="currentColor"
+                fill="transparent"
+                r="50"
+                cx="60"
+                cy="60"
+               />
+            </svg>
+    
+    
+    <div class="flex items-center flex-wrap max-w-md px-10  rounded-2xl h-20"
+       x-data="{ circumference: 50 * 2 * Math.PI, percent: 90 }"
+       >
+          <div class="flex items-center justify-center -m-6 overflow-hidden  rounded-full">
+            <svg class="w-32 h-32 transform translate-x-1 translate-y-1" x-cloak aria-hidden="true">
+              <circle
+                class="text-gray-300"
+                stroke-width="10"
+                stroke="currentColor"
+                fill="transparent"
+                r="50"
+                cx="60"
+                cy="60"
+                />
+              <circle
+                class="text-blue-600"
+                stroke-width="10"
+                :stroke-dasharray="circumference"
+                :stroke-dashoffset="circumference - percent / 100 * circumference"
+                stroke-linecap="round"
+                stroke="currentColor"
+                fill="transparent"
+                r="50"
+                cx="60"
+                cy="60"
+               />
+            </svg>
+
+            
+
+</div>
+
+
+
+
+
+
+<div class="fixed bottom-5 right-5 flex items-center space-x-4">
+  <a href="https://twitter.com/ak_kamona" target="_blank" class="transition-transform transform hover:scale-125">
+    <span class="sr-only">Twitter</span>
+      <svg
+         aria-hidden="true"
+         class="w-8 h-8 text-blue-500"
+         fill="currentColor"
+         xmlns="http://www.w3.org/2000/svg"
+         viewBox="0 0 24 24"
+            >
+      <path d="M19.633,7.997c0.013,0.175,0.013,0.349,0.013,0.523c0,5.325-4.053,11.461-11.46,11.461c-2.282,0-4.402-0.661-6.186-1.809 c0.324,0.037,0.636,0.05,0.973,0.05c1.883,0,3.616-0.636,5.001-1.721c-1.771-0.037-3.255-1.197-3.767-2.793 c0.249,0.037,0.499,0.062,0.761,0.062c0.361,0,0.724-0.05,1.061-0.137c-1.847-0.374-3.23-1.995-3.23-3.953v-0.05 c0.537,0.299,1.16,0.486,1.82,0.511C3.534,9.419,2.823,8.184,2.823,6.787c0-0.748,0.199-1.434,0.548-2.032 c1.983,2.443,4.964,4.04,8.306,4.215c-0.062-0.3-0.1-0.611-0.1-0.923c0-2.22,1.796-4.028,4.028-4.028 c1.16,0,2.207,0.486,2.943,1.272c0.91-0.175,1.782-0.512,2.556-0.973c-0.299,0.935-0.936,1.721-1.771,2.22 c0.811-0.088,1.597-0.312,2.319-0.624C21.104,6.712,20.419,7.423,19.633,7.997z"
+            ></path>
+    </svg>
+  </a>
+  <a href="https://github.com/Kamona-WD" target="_blank" class="transition-transform transform hover:scale-125">
+    <span class="sr-only">Github</span>
+      <svg
+         aria-hidden="true"
+         class="w-8 h-8 text-black"
+         fill="currentColor"
+         xmlns="http://www.w3.org/2000/svg"
+         viewBox="0 0 24 24"
+         >
+        <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M12.026,2c-5.509,0-9.974,4.465-9.974,9.974c0,4.406,2.857,8.145,6.821,9.465 c0.499,0.09,0.679-0.217,0.679-0.481c0-0.237-0.008-0.865-0.011-1.696c-2.775,0.602-3.361-1.338-3.361-1.338 c-0.452-1.152-1.107-1.459-1.107-1.459c-0.905-0.619,0.069-0.605,0.069-0.605c1.002,0.07,1.527,1.028,1.527,1.028 c0.89,1.524,2.336,1.084,2.902,0.829c0.091-0.645,0.351-1.085,0.635-1.334c-2.214-0.251-4.542-1.107-4.542-4.93 c0-1.087,0.389-1.979,1.024-2.675c-0.101-0.253-0.446-1.268,0.099-2.64c0,0,0.837-0.269,2.742,1.021 c0.798-0.221,1.649-0.332,2.496-0.336c0.849,0.004,1.701,0.115,2.496,0.336c1.906-1.291,2.742-1.021,2.742-1.021 c0.545,1.372,0.203,2.387,0.099,2.64c0.64,0.696,1.024,1.587,1.024,2.675c0,3.833-2.33,4.675-4.552,4.922 c0.355,0.308,0.675,0.916,0.675,1.846c0,1.334-0.012,2.41-0.012,2.737c0,0.267,0.178,0.577,0.687,0.479 C19.146,20.115,22,16.379,22,11.974C22,6.465,17.535,2,12.026,2z"
+              ></path>
+    </svg>
+  </a>
+</div>
+
+
+</div>
+
+
+                </div>
