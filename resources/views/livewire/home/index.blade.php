3c99da994239061ca771d94dd77935cc93592f1d
jerilphilippian <jerillabagnoy.philippian@gmail.com>
2023-11-02 18:31:34 +0800

    new

--- resources/views/livewire/home/index.blade.php
+++ resources/views/livewire/home/index.blade.php
@@ -13,8 +13,8 @@
             style="background-color: rgba(0, 0, 0, 0.75)">
             <div class="flex h-full items-center justify-center">
               <div class="px-6 text-center text-white md:px-12">
-                <h1 class="mb-6 text-5xl font-bold">Front End Web development</h1>
-                <h3 class="mb-8 text-3xl font-bold">Hello, I'am Jeril , Focusing on Creating Beautiful Design Experience.</h3>
+                <h1 class="mb-6 text-5xl font-bold">Welcome to my Portfolio</h1>
+                <h3 class="mb-8 text-3xl font-bold">Hello, I'am Jeril , Focusing on Functional Elements Creating Beautiful Design Experience.</h3>
                 <button
                   type="button"
                   class="inline-block rounded border-2 border-neutral-50 px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-neutral-50 transition duration-150 ease-in-out hover:border-neutral-100 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-neutral-100 focus:border-neutral-100 focus:text-neutral-100 focus:outline-none focus:ring-0 active:border-neutral-200 active:text-neutral-200 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
@@ -32,7 +32,7 @@
         <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
             <div class="mr-auto place-self-center lg:col-span-7">
                 <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">About Me</h1>
-                <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">I'm Jeril, and I really want to do front-end development. I have experience in designing using HTML, CSS, JavaScript, Tailwind, Laravel, Livewire, Rappasoft PowerGrid, animation, Bootstrap, Figma, and I can also work on network-related tasks.</p>
+                <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">I'm Jeril, and I really want to do front-end development. I have experience using HTML, CSS, JavaScript, Tailwind, Laravel, Livewire, Rappasoft PowerGrid, animation, Bootstrap, Figma, and I can also work on network-related tasks.</p>
                 <a href="#" class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                     Get started
                     <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
